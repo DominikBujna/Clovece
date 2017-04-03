94516c6241ee450b997d7d3b8e845474804cd0bc
Dominik <dominikb94@gmail.com>
2017-04-03 14:43:57 +0200

    Vsetkym pocita jedno skore, koniec hry treba vyriesit, hadzanie kockou opravit

--- clovece.py
+++ clovece.py
@@ -18,33 +18,37 @@ class Player:
 # pole hracov
 hraci = [Player("Dominik"), Player("Jozko")]
 # hra
-game_on = False
+game_on = True
 while (game_on):
     for hrac in hraci:
         # hodim kockou
         hod = randint(1, 6)
         nahadzane = hod + 0
         print(hrac.name, " hodil ", hod)
+        print(hrac.player_position)
         # ak hodim 6, hadzem dalej
         while (hod == 6):
             hod = randint(1, 6)
             nahadzane += hod
+            print(hrac.name, " hodil este: ", hod)
         # ak som na ploche, vyberiem kym idem
-        if -6 not in hrac.player_position:
-            # vyberiem ktoreho hraca chcem
-            tah = int(input("Vyberte hraca od 1 po ", num_of_figures + 1)) % num_of_figures
-            # vykonam tah zvolenym hracom, ak nie je v dome
-            if (hrac.player_position[tah + 1] + nahadzane > board_size):
-                print("Nemozem vykonat tah.")
+        # vyberiem ktoreho hraca chcem
+        tah = int(input("Vyberte hraca")) % num_of_figures
+        # vykonam tah zvolenym hracom, ak nie je v dome
+        if (hrac.player_position[tah - 1] + nahadzane > board_size):
+            print("Nemozem vykonat tah.")
+        else:
+            if (hrac.player_position[tah - 1] == -6 and nahadzane < 6):
+                tah = int(input("Hrac nie je v hre."))
             else:
-                hrac.player_position[tah + 1] += nahadzane
-            # pozrem, ci je pole volne
-            for protihrac in hraci:
-                for pozicia in protihrac.player_position:
+                hrac.player_position[tah - 1] += nahadzane
+        # pozrem, ci je pole volne
+        for protihrac in hraci:
+            for pozicia in protihrac.player_position:
                     # ak je pole plne, vyhodim ho
                     if (hrac.player_position[tah] == pozicia and protihrac != hrac):
                         pozicia = -6
                         # ak som tymto tahom skoncil
-            if (i for i in hrac.player_position if i > (board_size - num_of_figures)):
-                game_on = False
-                print(hrac, " vyhral hru.")
+        if (hrac.player_position[tah] >= board_size - num_of_figures):
+            game_on = False
+            print(hrac.name, " vyhral hru. ")
