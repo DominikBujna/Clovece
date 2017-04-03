69089ea8aea1a9a3d787988d760b84bd42c614a3
Dominik <dominikb94@gmail.com>
2017-04-03 14:15:49 +0200

    Prva verzia, pred prym spustenim, domov nefunguje na obsadene policka doma, vsetci hraci zacinaju na rovnakom mieste

--- clovece.py
+++ clovece.py
@@ -3,7 +3,7 @@ from random import randint
 # globalne premenne
 num_of_players = 2
 num_of_figures = 4
-board_size = 180
+board_size = 179
 
 
 # udrzujem info o hracoch
@@ -18,20 +18,33 @@ class Player:
 # pole hracov
 hraci = [Player("Dominik"), Player("Jozko")]
 # hra
-
-while (True):
+game_on = False
+while (game_on):
     for hrac in hraci:
+        # hodim kockou
         hod = randint(1, 6)
+        nahadzane = hod + 0
         print(hrac.name, " hodil ", hod)
-        if (hrac.on_board and hod == 6):
-            hrac.player_position
+        # ak hodim 6, hadzem dalej
+        while (hod == 6):
+            hod = randint(1, 6)
+            nahadzane += hod
+        # ak som na ploche, vyberiem kym idem
+        if -6 not in hrac.player_position:
             # vyberiem ktoreho hraca chcem
-            tah = int(input("Vyberte hraca")) % num_of_figures
-            hrac.player_position[tah] += hod
-            # TODO: vykonanie tahu - ceknut, ci je volne pole, posunut
+            tah = int(input("Vyberte hraca od 1 po ", num_of_figures + 1)) % num_of_figures
+            # vykonam tah zvolenym hracom, ak nie je v dome
+            if (hrac.player_position[tah + 1] + nahadzane > board_size):
+                print("Nemozem vykonat tah.")
+            else:
+                hrac.player_position[tah + 1] += nahadzane
             # pozrem, ci je pole volne
             for protihrac in hraci:
                 for pozicia in protihrac.player_position:
                     # ak je pole plne, vyhodim ho
                     if (hrac.player_position[tah] == pozicia and protihrac != hrac):
                         pozicia = -6
+                        # ak som tymto tahom skoncil
+            if (i for i in hrac.player_position if i > (board_size - num_of_figures)):
+                game_on = False
+                print(hrac, " vyhral hru.")
